b7b6c0690a52dce53b8e0c897d6f53a278b86255
Manille Desseaux <manille024@gmail.com>
2025-02-14 09:56:33 +0100

    class order

---
+++ Royal Restaurant/src/Order.java
@@ -0,0 +1,44 @@
+
+public class Order {
+    private int numOrder;
+    private String dishes;
+    private float Total;
+    
+    public Order (int numOrder, String dishes, float Total) {
+
+        this.numOrder = numOrder ;
+        this.dishes = dishes ;
+        this.Total = Total ;
+    }
+
+    public int getNumOrder() {
+        return numOrder;
+    }
+
+    public void setNumOrder(int numOrder) {
+        this.numOrder = numOrder;
+    }
+
+    public String getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(String dishes) {
+        this.dishes = dishes;
+    }
+
+    public float getTotal() {
+        return Total;
+    }
+
+    public void setTotal(float Total) {
+        this.Total = Total;
+    }
+
+    public String toString() {
+        String answer = "Menu : " + numOrder
+                        + ", Date de création : " + dishes
+                        + ", Type de menu : " + Total ;
+        return answer;
+    }
+}
